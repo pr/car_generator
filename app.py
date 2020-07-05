c68829ef05c1bc3940ebf4c1e238ed03f7d785e0
Peter Richards <peter@peter.net>
2020-07-05 14:57:44 -0400

    hmmmm

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ flask_app = Flask(__name__)
 @flask_app.route('/')
 def get_image():
     subprocess.call('main.py', shell=True)
-    return send_file('./car.png', mimetype='image/png')
+    return send_file('app/car.png', mimetype='image/png')
 
 
 if __name__ == "__main__":
